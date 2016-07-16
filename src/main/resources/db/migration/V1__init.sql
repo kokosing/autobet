a9cbd21d629d8f51a30b44fe0a8f29e25d35a22b
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-07-16 21:06:26 +0200

    Add bet related models

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -5,8 +5,7 @@ CREATE TABLE divisions (
 CREATE TABLE teams (
     id bigint auto_increment,
     division_id bigint,
-    name varchar(128),
-);
+    name varchar(128));
 
 CREATE TABLE games (
     id bigint auto_increment,
@@ -40,7 +39,46 @@ CREATE TABLE games (
     home_team_yellow_cards tinyint,
     away_team_yellow_cards tinyint,
     home_team_red_cards tinyint,
-    away_team_red_cards tinyint
-);
+    away_team_red_cards tinyint);
 
 CREATE INDEX games_load_index ON games (home_team_id, away_team_id, played_at);
+
+CREATE TABLE bet_types (
+    id bigint auto_increment,
+    name varchar(128),
+    expression varchar(2048),
+    bet_suffix varchar(4));
+
+INSERT INTO bet_types VALUES
+    (1, 'full_time_home_win', 'full_time_home_team_goals > full_time_away_team_goals', 'H'),
+    (2, 'full_time_away_win', 'full_time_home_team_goals < full_time_away_team_goals', 'A'),
+    (3, 'full_time_draw',     'full_time_home_team_goals = full_time_away_team_goals', 'D');
+
+CREATE TABLE bet_vendors (
+    id bigint auto_increment,
+    name varchar(128),
+    bet_prefix varchar(16));
+
+INSERT INTO bet_vendors VALUES
+    (1, 'Bet365', 'B365'),
+    (2, 'Blue Square', 'BS'),
+    (3, 'Bet&Win', 'BW'),
+    (4, 'Gamebookers', 'GB'),
+    (5, 'Interwetten', 'IW'),
+    (6, 'Ladbrokes', 'LB'),
+    (7, 'Pinnacle Sports', 'PS'),
+    (8, 'Sporting Odds', 'SO'),
+    (9, 'Sportingbet', 'SB'),
+    (10, 'Stan James', 'SJ'),
+    (11, 'Stanleybet', 'SY'),
+    (12, 'VC Bet', 'VC'),
+    (13, 'William Hill', 'WH');
+
+CREATE TABLE bets (
+    id bigint auto_increment,
+    bet_vendor_id bigint,
+    bet_type_id bigint,
+    game_id bigint,
+    odds decimal(10,4));
+
+CREATE INDEX bet_by_game_index ON bets (game_id);
