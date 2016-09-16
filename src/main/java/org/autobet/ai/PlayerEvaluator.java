01409720af7a7b9e56bd5f8da832a35b3e3a71da
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-09-16 15:00:19 +0200

    Remove unused code

--- src/main/java/org/autobet/ai/PlayerEvaluator.java
+++ src/main/java/org/autobet/ai/PlayerEvaluator.java
@@ -16,22 +16,15 @@ package org.autobet.ai;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.collect.Iterables;
 import org.autobet.model.Bet;
 import org.autobet.model.Game;
 import org.autobet.util.GamesProcessorDriver;
 import org.autobet.util.KeyValueStore;
-import org.javalite.activejdbc.Base;
-import org.javalite.activejdbc.RowListener;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkState;
-import static java.util.Objects.requireNonNull;
 
 public class PlayerEvaluator
 {
@@ -109,35 +102,6 @@ public class PlayerEvaluator
                     other.getStorageKey());
 
             return new Statistics(storageKey, result + other.getResult());
-        }
-    }
-
-    private static <T> T querySingleValue(String query, Object... params)
-    {
-        SingleValueRowProcessor singleValueRowProcessor = new SingleValueRowProcessor();
-        Base.find(query, params)
-                .with(singleValueRowProcessor);
-        T value = singleValueRowProcessor.get();
-        return (T) requireNonNull(value, () -> "got null for: '" + query + "' with params: " + Arrays.toString(params));
-    }
-
-    private static class SingleValueRowProcessor
-            implements RowListener
-    {
-        Object value;
-
-        @Override
-        public boolean next(Map<String, Object> row)
-        {
-            checkState(value == null, "Query returned to many rows");
-            checkState(row.size() == 1, "Query returned to many columns");
-            value = Iterables.getOnlyElement(row.values());
-            return true;
-        }
-
-        public <T> T get()
-        {
-            return (T) value;
         }
     }
 }
