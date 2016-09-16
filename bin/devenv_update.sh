7c2572466206c7b23db0c2ebf0cdc8af36471894
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-09-16 19:13:11 +0200

    Revert "Drop key_value_store_entries when updating image"
    This reverts commit 54be0ce003d4678e5721eb09107423ee34d622f5.

--- bin/devenv_update.sh
+++ bin/devenv_update.sh
@@ -4,7 +4,6 @@ source bin/devenv_start.sh
 
 sleep 20
 
-echo 'delete from key_value_store_entries;' | docker exec -i mysql_autobet mysql -pmysql autobet
 java -jar target/autobet-0.1-SNAPSHOT-executable.jar stats -c 10000
 java -jar target/autobet-0.1-SNAPSHOT-executable.jar eval -c 1000 -s random
 java -jar target/autobet-0.1-SNAPSHOT-executable.jar eval -c 1000 -s goal_based
