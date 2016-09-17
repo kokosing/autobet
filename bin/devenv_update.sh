2d192891a1b315b40c075ae307c02726f7c0a859
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-09-17 23:00:31 +0200

    Make the image update step to be smaller

--- bin/devenv_update.sh
+++ bin/devenv_update.sh
@@ -4,6 +4,6 @@ source bin/devenv_start.sh
 
 sleep 20
 
-java -jar target/autobet-0.1-SNAPSHOT-executable.jar stats -c 10000
-java -jar target/autobet-0.1-SNAPSHOT-executable.jar eval -c 1000 -s random
-java -jar target/autobet-0.1-SNAPSHOT-executable.jar eval -c 1000 -s goal_based
+java -jar target/autobet-0.1-SNAPSHOT-executable.jar stats -c 5000
+java -jar target/autobet-0.1-SNAPSHOT-executable.jar eval -c 500 -s random
+java -jar target/autobet-0.1-SNAPSHOT-executable.jar eval -c 500 -s goal_based
