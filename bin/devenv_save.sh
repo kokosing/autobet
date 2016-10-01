054e7cd97514a6018ba59b94ba2f3a9627ce2dd7
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-10-01 22:42:48 +0200

    Squash docker layers via mysql

---
+++ bin/devenv_save.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+if ! docker ps  | grep -q mysql_autobet$; then
+  echo devenv is not started
+  exit
+fi
+
+docker exec -i mysql_autobet mysqldump -pmysql autobet > dump.sql
+
+docker rm -f mysql_autobet_tmp
+docker run --name mysql_autobet_tmp -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_USER=mysql -e MYSQL_PASSWORD=mysql -d kokosing/mysql
+
+sleep 30
+
+echo "create database autobet;" | docker exec -i mysql_autobet_tmp mysql -pmysql mysql
+docker exec -i mysql_autobet_tmp mysql -pmysql autobet < dump.sql
+
+docker commit mysql_autobet_tmp kokosing/mysql_autobet:latest
+docker login -u="$DOCKER_LOGIN" -p="$DOCKER_PASSWORD"
+docker push kokosing/mysql_autobet:latest
